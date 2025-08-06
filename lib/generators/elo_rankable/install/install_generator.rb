2e15cd64cf5a1df2d62f80b4ef01c1c1ab7d80c6
Aberen <nijoergensen@gmail.com>
2025-08-06 23:21:53 +0200

    chore: fixed generator code.

--- lib/generators/elo_rankable/install/install_generator.rb
+++ lib/generators/elo_rankable/install/install_generator.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
-require 'rails/generators/active_record'
+require 'rails/generators'
+require 'rails/generators/migration'
 
 module EloRankable
   module Generators
-    class InstallGenerator < ActiveRecord::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
       desc 'Create migration for EloRankable'
 
       source_root File.expand_path('templates', __dir__)
+
+      def self.next_migration_number(dirname)
+        next_migration_number = current_migration_number(dirname) + 1
+        ActiveRecord::Migration.next_migration_number(next_migration_number)
+      end
 
       def self.default_generator_root
         File.dirname(__FILE__)
